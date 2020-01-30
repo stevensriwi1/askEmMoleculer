359095ecb6a1f7fefeff2e1f4f40133ba55da015
steven <steven_sriwi@hotmail.com>
2020-01-30 19:38:03 +1100

    initial commit for moleculer

---
+++ services/greeter.service.js
@@ -0,0 +1,85 @@
+"use strict";
+
+//var admin = require("firebase");
+
+var serviceAccount = require("../askem-flutter-firebase-adminsdk-ckzgy-7e32cf7574.json");
+
+module.exports = {
+	name: "greeter",
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 *
+		 * @returns
+		 */
+		hello() {
+			return "Hello Moleculer";
+		},
+
+		/**
+		 * Welcome a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			params: {
+				name: "string"
+			},
+			handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+		console.log("helo");
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
